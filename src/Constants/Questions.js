bfb8d9e90f726d94af3bd42f3f1743735d3148d2
Rory Flint <rory.flint@cpandr.co.uk>
2020-11-14 23:47:29 +0000

    Add all 70 questions

--- src/Constants/Questions.js
+++ src/Constants/Questions.js
@@ -55,4 +55,337 @@ export const temperamentQuestions = [
     question: 'Are you more interested in...',
     answers: ['what is actual?', 'what is possible?'],
   },
+  {
+    number: 11,
+    question: 'In judging others are you more swayed by...',
+    answers: ['laws than circumstances?', 'circumstances than laws?'],
+  },
+  {
+    number: 12,
+    question: 'In approaching others is your inclination to be somewhat...',
+    answers: ['objective (detached)?', 'personal?'],
+  },
+  {
+    number: 13,
+    question: 'Are you more...',
+    answers: ['punctual?', 'leisurely?'],
+  },
+  {
+    number: 14,
+    question: 'Does it bother you more having things...',
+    answers: ['incomplete?', 'completed?'],
+  },
+  {
+    number: 15,
+    question: 'In your social groups do you...',
+    answers: ["keep abreast of other's happenings?", 'get behind on news?'],
+  },
+  {
+    number: 16,
+    question: 'In doing ordinary things are you more likely to...',
+    answers: ['do it the usual way?', 'do it your own way?'],
+  },
+  {
+    number: 17,
+    question: 'Writers should...',
+    answers: [
+      '"say what they mean and mean what they say"?',
+      'express things more by use of analogy?',
+    ],
+  },
+  {
+    number: 18,
+    question: 'Which appeals to you more?',
+    answers: ['Consistency of thought', 'Harmonious human relationships'],
+  },
+  {
+    number: 19,
+    question: 'Are you more comfortable in making...',
+    answers: ['logical judgements?', 'value judgements?'],
+  },
+  {
+    number: 20,
+    question: 'Do you want things...',
+    answers: ['settled and decided?', 'unsettled and undecided?'],
+  },
+  {
+    number: 21,
+    question: 'Would you say you are more...',
+    answers: ['serious and detemined?', 'easy-going?'],
+  },
+  {
+    number: 22,
+    question: 'In phoning do you...',
+    answers: [
+      'rarely question that it will all be said?',
+      "rehearse what you'll say?",
+    ],
+  },
+  {
+    number: 23,
+    question: 'Facts...',
+    answers: ['"speak for themselves', 'illustrate principles'],
+  },
+  {
+    number: 24,
+    question: 'Are visionaries...',
+    answers: ['"somewhat annoying?', 'rather fascinating?'],
+  },
+  {
+    number: 25,
+    question: 'Are you more often...',
+    answers: ['a cool-headed person?', 'a warm-hearted person?'],
+  },
+  {
+    number: 26,
+    question: 'It is worse to be...',
+    answers: ['unjust?', 'merciless?'],
+  },
+  {
+    number: 27,
+    question: 'Should one usually let events occur...',
+    answers: ['by careful selection and choice?', 'randomly and by chance?'],
+  },
+  {
+    number: 28,
+    question: 'Do you feel better about...',
+    answers: ['having purchased?', 'having the option to buy?'],
+  },
+  {
+    number: 29,
+    question: 'In company do you...',
+    answers: ['initiate conversation?', 'wait to be approached?'],
+  },
+  {
+    number: 30,
+    question: 'Common sense is...',
+    answers: ['rarely questionable?', 'frequently questionable?'],
+  },
+  {
+    number: 31,
+    question: 'Children often do not...',
+    answers: [
+      'make themselves useful enough?',
+      'exercise their fantasy enough?',
+    ],
+  },
+  {
+    number: 32,
+    question: 'In making decisions do you feel more comfortable with...',
+    answers: ['standards?', 'feelings?'],
+  },
+  {
+    number: 33,
+    question: 'Are you more...',
+    answers: ['firm than gentle?', 'gentle than firm?'],
+  },
+  {
+    number: 34,
+    question: 'Which is more admirable...',
+    answers: [
+      'the ability to organise and be methodical?',
+      'the ability to adapt and make do?',
+    ],
+  },
+  {
+    number: 35,
+    question: 'Do you put more value on the...',
+    answers: ['definite?', 'open-ended?'],
+  },
+  {
+    number: 36,
+    question: 'Does new and non-routine interaction with others...',
+    answers: ['stimulate and energise you?', 'tax your reserves?'],
+  },
+  {
+    number: 37,
+    question: 'Are you more frequently...',
+    answers: ['a practical sort of person?', 'a fanciful sort of person?'],
+  },
+  {
+    number: 38,
+    question: 'Are you more likey to...',
+    answers: ['see how others are useful?', "see how other's see?"],
+  },
+  {
+    number: 39,
+    question: 'Which is more satisfying...',
+    answers: [
+      'to discuss an issue thoroughly?',
+      'to arrive at agreement on an issue?',
+    ],
+  },
+  {
+    number: 40,
+    question: 'Which rules you more?',
+    answers: ['Your head', 'Your heart'],
+  },
+  {
+    number: 41,
+    question: 'Are you more comfortable with work that is...',
+    answers: ['contracted?', 'done on a casual basis?'],
+  },
+  {
+    number: 42,
+    question: 'Do you tend to look for...',
+    answers: ['the orderly?', 'whatever turns up?'],
+  },
+  {
+    number: 43,
+    question: 'Do you prefer...',
+    answers: [
+      'many friends with brief contact?',
+      'a few friends with more lengthy contact?',
+    ],
+  },
+  {
+    number: 44,
+    question: 'Do you go more by...',
+    answers: ['facts?', 'principles?'],
+  },
+  {
+    number: 45,
+    question: 'Are you more interested in...',
+    answers: ['production and distribution?', 'design and research?'],
+  },
+  {
+    number: 46,
+    question: 'Which is more of a compliment?',
+    answers: [
+      '"You are a very logical person."',
+      'You are a very sentimental person.',
+    ],
+  },
+  {
+    number: 47,
+    question: 'Do you value in yourself more that you are...',
+    answers: ['unwavering?', 'devoted?'],
+  },
+  {
+    number: 48,
+    question: 'Do you more often prefer the...',
+    answers: [
+      'final and unalterable statement?',
+      'tentative and preliminary statement?',
+    ],
+  },
+  {
+    number: 49,
+    question: 'Are you more comfortable...',
+    answers: ['after a decision?', 'before a decision?'],
+  },
+  {
+    number: 50,
+    question: 'Do you...',
+    answers: [
+      'speak easily and at length with strangers?',
+      'find little to say to strangers?',
+    ],
+  },
+  {
+    number: 51,
+    question: 'Are you more likely to trust your...',
+    answers: ['experience?', 'hunch?'],
+  },
+  {
+    number: 52,
+    question: 'Do you feel...',
+    answers: [
+      'more practical than ingenious?',
+      'more ingenious than practical?',
+    ],
+  },
+  {
+    number: 53,
+    question: 'Which person is more to be complimented, one of...',
+    answers: ['clear reason?', 'strong feeling?'],
+  },
+  {
+    number: 54,
+    question: 'Are you inclined more to be...',
+    answers: ['fair minded?', 'sympathetic?'],
+  },
+  {
+    number: 55,
+    question: 'Is it preferable mostly to...',
+    answers: ['make sure things are arranged?', 'just let things happen?'],
+  },
+  {
+    number: 56,
+    question: 'In relationships should most things be...',
+    answers: ['renegotiable?', 'random and circumstantial?'],
+  },
+  {
+    number: 57,
+    question: 'When the phone rings do you...',
+    answers: ['hasten to get to it first?', 'hope somebody else will answer?'],
+  },
+  {
+    number: 58,
+    question: 'Do you prize more in yourself...',
+    answers: ['a strong sense of reality?', 'a vivid imagination?'],
+  },
+  {
+    number: 59,
+    question: 'Are you drawn more to...',
+    answers: ['fundamentals?', 'overtones?'],
+  },
+  {
+    number: 60,
+    question: 'Which seems the greater error...',
+    answers: ['to be too passionate?', 'to be too objective?'],
+  },
+  {
+    number: 61,
+    question: 'Do you see yourself as basically...',
+    answers: ['hard-headed?', 'soft-hearted?'],
+  },
+  {
+    number: 62,
+    question: 'Which situation appeals to you more...',
+    answers: [
+      'the structured and the scheduled?',
+      'the unstructured and unscheduled?',
+    ],
+  },
+  {
+    number: 63,
+    question: 'Are you a person that is more...',
+    answers: ['routinized than whimsica?', 'whimsical than routinized?'],
+  },
+  {
+    number: 64,
+    question: 'Are you more inclined to be...',
+    answers: ['easy to approach?', 'somewhat reserved?'],
+  },
+  {
+    number: 65,
+    question: 'In writings do you prefer...',
+    answers: ['the more literal?', 'the more figurative?'],
+  },
+  {
+    number: 66,
+    question: 'Is it harder for you to...',
+    answers: ['identify with others?', 'utilise others?'],
+  },
+  {
+    number: 67,
+    question: 'Which do you wish more for yourself?',
+    answers: ['Clarity of reason', 'Strength of compassion'],
+  },
+  {
+    number: 68,
+    question: 'Which is the greater fault?',
+    answers: ['Being indiscriminate', 'Being critical'],
+  },
+  {
+    number: 69,
+    question: 'Do you prefer the...',
+    answers: ['planned event?', 'unplanned event?'],
+  },
+  {
+    number: 70,
+    question: 'Do you tend to be more...',
+    answers: ['deliberate than spontaneous?', 'spontaneous than deliberate?'],
+  },
 ];

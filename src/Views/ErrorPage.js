ab3ec145b38589f5aa81cf520628e25f6f4b60f9
Rory Flint <rory.flint@tipalti.com>
2025-07-04 12:28:39 +0100

    --

--- src/Views/ErrorPage.js
+++ src/Views/ErrorPage.js
@@ -1,14 +1,25 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { AnswersContext } from "../Context/AnswersContext";
 
 const ErrorPage = () => {
+  const { setUserAnswers } = useContext(AnswersContext);
+  const history = useHistory();
+
+  const handleReset = () => {
+    // Clear the context
+    setUserAnswers({});
+
+    // Redirect to homepage
+    history.push("/");
+  };
+
   return (
     <StyledErrorContainer>
       <h1>Uh oh!</h1>
       <p className="first">Looks like you've taken a wrong turn.</p>
-      <p>
-        If you work for CP+R, please follow the link from The Hub to get your
-        unique access token.
-      </p>
+      <ResetButton onClick={handleReset}>Reset & Start Over</ResetButton>
     </StyledErrorContainer>
   );
 };
@@ -38,6 +49,27 @@ const StyledErrorContainer = styled.div`
 
       color: ${(props) => props.theme.darkgrey};
     }
+  }
+`;
+
+const ResetButton = styled.button`
+  background-color: ${(props) => props.theme.orange};
+  color: white;
+  border: none;
+  padding: 1rem 2rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+  border-radius: 8px;
+  cursor: pointer;
+  margin-top: 2rem;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: ${(props) => props.theme.darkgrey};
+  }
+
+  &:active {
+    transform: translateY(1px);
   }
 `;
 
